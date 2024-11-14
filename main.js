db27c6a057d9eb15ceb662e703f83487636cdb53
huachuman <125603964+huachuman@users.noreply.github.com>
2024-11-14 13:55:36 -0500

    add navigate

--- main.js
+++ main.js
@@ -2,9 +2,61 @@
 
 const obsidian = require('obsidian');
 
-module.exports = class DeleteWordPlugin extends obsidian.Plugin {
+module.exports = class WordNavigationPlugin extends obsidian.Plugin {
     async onload() {
-        // Delete word forward
+        // Move word forward
+        this.addCommand({
+            id: 'move-word-forward',
+            name: 'Move word forward',
+            editorCallback: (editor) => {
+                const cursor = editor.getCursor();
+                const line = editor.getLine(cursor.line);
+                const text = line.slice(cursor.ch);
+                
+                const match = text.match(/^\s*\S+\s*/);
+                if (match) {
+                    const moveLength = match[0].length;
+                    editor.setCursor({
+                        line: cursor.line,
+                        ch: cursor.ch + moveLength
+                    });
+                } else if (cursor.line < editor.lineCount() - 1) {
+                    // Move to start of next line if at end of current line
+                    editor.setCursor({
+                        line: cursor.line + 1,
+                        ch: 0
+                    });
+                }
+            }
+        });
+
+        // Move word backward
+        this.addCommand({
+            id: 'move-word-backward',
+            name: 'Move word backward',
+            editorCallback: (editor) => {
+                const cursor = editor.getCursor();
+                const line = editor.getLine(cursor.line);
+                const text = line.slice(0, cursor.ch);
+                
+                const match = text.match(/\s*\S+\s*$/);
+                if (match) {
+                    const moveLength = match[0].length;
+                    editor.setCursor({
+                        line: cursor.line,
+                        ch: cursor.ch - moveLength
+                    });
+                } else if (cursor.line > 0) {
+                    // Move to end of previous line if at start of current line
+                    editor.setCursor({
+                        line: cursor.line - 1,
+                        ch: editor.getLine(cursor.line - 1).length
+                    });
+                }
+            }
+        });
+
+        // Delete word forward (original)
         this.addCommand({
             id: 'delete-word-forward',
             name: 'Delete word forward',
@@ -25,7 +77,7 @@ module.exports = class DeleteWordPlugin extends obsidian.Plugin {
             }
         });
 
-        // Delete word backward
+        // Delete word backward (original)
         this.addCommand({
             id: 'delete-word-backward',
             name: 'Delete word backward',
